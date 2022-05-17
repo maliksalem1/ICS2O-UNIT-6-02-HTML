b8390465b0c749f3c5b1dade823bb2ec163e246d
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-17 08:11:02 -0400

    added javascript

--- js/script.js
+++ js/script.js
@@ -8,12 +8,34 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-6-02-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-6-02-HTML/",
   })
 }
 
 /**
- * Input.
+ * Input
  */
 
+ function updateCount() {
+  if (localStorage.clicks) {
+    document.getElementById("output").innerHTML = "Cookies: " + localStorage.clicks
+  } else {
+    localStorage.clicks = 0
+  }
+}
+/**
+ * Resets the click counter
+ */
+function resetProgress() {
+  localStorage.clicks = 0
+  updateCount()
+}
+
+/**
+ * Adds 1 to the counter
+ */
+function onClick() {
+  localStorage.clicks++
+  updateCount()
+}
