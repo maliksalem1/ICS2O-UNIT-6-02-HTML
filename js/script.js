6f8b93c17eac1923cc83f8f762313b2791f13f1d
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-18 12:10:53 -0400

    removed images folder

--- js/script.js
+++ js/script.js
@@ -2,40 +2,40 @@
    Created on: April 2022
    This file contains the JS functions for index.html*/
 
-'use strict'
+   'use strict'
 
-/**
- * Check service worker.
- */
- if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-UNIT-6-02-HTML/sw.js", {
-    scope: "/ICS2O-UNIT-6-02-HTML/",
-  })
-}
-
-/**
- * Input
- */
-
- function updateCount() {
-  if (localStorage.clicks) {
-    document.getElementById("output").innerHTML = "Cookies: " + localStorage.clicks
-  } else {
-    localStorage.clicks = 0
-  }
-}
-/**
- * Resets the click counter
- */
-function resetCookies() {
-  localStorage.clicks = 0
-  updateCount()
-}
-
-/**
- * Adds 1 to the counter
- */
-function getCookies() {
-  localStorage.clicks++
-  updateCount()
-}
+   /**
+    * Check service worker.
+    */
+    if (navigator.serviceWorker) {
+     navigator.serviceWorker.register("/ICS2O-UNIT-6-02-HTML/sw.js", {
+       scope: "/ICS2O-UNIT-6-02-HTML/",
+     })
+   }
+   
+   /**
+    * Input
+    */
+   
+    function updateCount() {
+     if (localStorage.clicks) {
+       document.getElementById("output").innerHTML = "Cookies: " + localStorage.clicks
+     } else {
+       localStorage.clicks = 0
+     }
+   }
+   /**
+    * Resets the click counter
+    */
+   function resetCookies() {
+     localStorage.clicks = 0
+     updateCount()
+   }
+   
+   /**
+    * Adds 1 to the counter
+    */
+   function getCookies() {
+     localStorage.clicks++
+     updateCount()
+   }
